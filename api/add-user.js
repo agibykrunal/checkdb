be6372d87f3f12a3e7c0dc5f5f1f22d594e990da
Krunal <nsundersia@gmail.com>
2026-05-09 14:22:13 +0530

    Update add-user.js

--- api/add-user.js
+++ api/add-user.js
@@ -10,7 +10,7 @@ module.exports = async (req, res) => {
   res.setHeader('Content-Type', 'application/json');
 
   if (req.method === 'POST') {
-    // ── ADD a new use
+    // ── ADD a new us
     try {
       const { name, email } = req.body;
 
