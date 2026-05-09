640dcd36c856fe3f2c7404fdaae4a7954a94e274
Krunal <nsundersia@gmail.com>
2026-05-09 14:24:58 +0530

    Update add-user.js

--- api/add-user.js
+++ api/add-user.js
@@ -32,7 +32,7 @@ module.exports = async (req, res) => {
     }
 
   } else if (req.method === 'GET') {
-    // ── GET all use
+    // ── GET all us
     try {
       const result = await pool.query('SELECT * FROM app_users ORDER BY id');
       res.status(200).json({
